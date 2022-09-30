0f5da84ba1325ca9ba5c7cf55c1343a8f33d93a0
Tyler Esselman <tme91302@gmail.com>
2022-09-29 19:38:14 -0700

    set up initial file and folder structure, built first routes

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const { readFromFile, readAndAppend } = require('./helpers/fsUtils');
+
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
+app.use(express.static('public'));
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.get('/', (req, res) => 
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.get('/api/notes', (req, res) => {
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))    
+});
+
+app.post('./api/notes', (req, res) => {
+    const { title, text } = req.body;
+    if (title && text)
+})
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
+
+
